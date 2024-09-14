daa084c137115887af78e54affaa2e4e42969f6b
Isheeta-design <Isheetasharma14@gmail.com>
2024-09-14 22:36:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const images = document.querySelectorAll("img");
 
 for( const image of images) {
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch("https://www.istockphoto.com/photo/futuristic-digital-blockchain-background-fintech-technology-gm1060950942-283607342?utm_campaign=srp_portrait_photos_40&utm_content=https%3A%2F%2Fwww.pexels.com%2Fsearch%2FFintech%2F&utm_medium=affiliate&utm_source=pexels&utm_term=Fintech")
     .then(response => response.json())
     .then(data => {
         image.src = data.message,
